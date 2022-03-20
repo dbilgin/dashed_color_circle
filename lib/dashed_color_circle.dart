36adde9ab31a6043441ae202d6d94df91f587324
dbilgin <denizcanbilgin@gmail.com>
2022-03-21 00:12:18 +0100

    added documentation comments

--- lib/dashed_color_circle.dart
+++ lib/dashed_color_circle.dart
@@ -4,6 +4,14 @@ import 'package:flutter/material.dart';
 import 'dash_painter.dart';
 
 class DashedColorCircle extends StatelessWidget {
+  /// Adding [dashes] will increase the dash count, but
+  /// if you add too many, you might have to change the
+  /// [gapSize] which would move the dashes further away from
+  /// each other.
+  /// [emptyColor] represents the default color of the dashes.
+  /// If you declare [fillCount] together with [filledColor]
+  /// you would get the amount of dashes with [fillColor] in the
+  /// specified color.
   const DashedColorCircle({
     Key? key,
     this.dashes = 3,
@@ -16,13 +24,28 @@ class DashedColorCircle extends StatelessWidget {
     this.strokeCap = StrokeCap.round,
   }) : super(key: key);
 
+  /// The count of dashes to be displayed
   final int dashes;
+
+  /// The default color of the dashes
   final Color emptyColor;
+
+  /// The color of the filled dashes. This will be equivalent to the [fillCount]
   final Color filledColor;
+
+  /// Distance between two dashes
   final double gapSize;
+
+  /// Width of the dashes
   final double strokeWidth;
+
+  /// Total size of the circle (width, height together)
   final double size;
+
+  /// Count of dashes to be colored with [filledColor]
   final double fillCount;
+
+  /// [StrokeCap] for the dash style
   final StrokeCap strokeCap;
 
   @override
